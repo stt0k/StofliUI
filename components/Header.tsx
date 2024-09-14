59f9d35dea4637841aa2d8225c0be0babb6ca155
unknown <espstok04@gmail.com>
2024-09-14 21:39:09 +0200

    added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+"use client"
+
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
-import { useState, useEffect } from "react"
+import { usePathname } from 'next/navigation'
 import { Sheet, SheetContent, SheetTrigger, SheetClose } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
 import { GithubIcon, TwitterIcon, MenuIcon, SearchIcon, MoonIcon, SunIcon, XIcon } from "lucide-react"
 import { Input } from "@/components/ui/input"
-
+import Tag from "@/components/ui/Tags"
+import SideBar from "@/components/Sidebar"
 
 const Header = () => {
   const [theme, setTheme] = useState("light")
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
   const [isSearchOpen, setIsSearchOpen] = useState(false)
   const [searchQuery, setSearchQuery] = useState("")
+  const pathname = usePathname()
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme") || "light"
@@ -32,15 +36,78 @@ const Header = () => {
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Implement your search logic here
     console.log('Searching for:', searchQuery);
     setIsSearchOpen(false);
     setSearchQuery('');
-  };  
+  };
+
+  const isActive = (href: string) => pathname === href;
+
+  const SideBar = () => (
+    <div className="h-full py-6 px-4 overflow-y-auto">
+      <div className="space-y-4">
+        {/* Introduction */}
+        <div className="py-1">
+          <h2 className="mb-2 px-2 text-base font-medium tracking-tight text-zinc-950 dark:text-zinc-50">Getting Started</h2>
+          <div className="space-y-1">
+            <Link href="/" onClick={closeMobileMenu}>
+              <Button
+                variant="ghost"
+                className={`w-full justify-start font-normal transition duration-200 hover:translate-x-1 ${isActive('/') ? 'text-sky-600 hover:text-sky-500' : ''}`}
+              >
+                Introduction
+              </Button>
+            </Link>
+            <Link href="/docs/installation" onClick={closeMobileMenu}>
+              <Button
+                variant="ghost"
+                className={`w-full justify-start font-normal transition duration-200 hover:translate-x-1 ${isActive('/docs/installation') ? 'text-sky-600 hover:text-sky-500' : ''}`}
+              >
+                Installation
+              </Button>
+            </Link>
+            <Link href="/docs/cli" onClick={closeMobileMenu}>
+              <Button
+                variant="ghost"
+                className={`w-full justify-start font-normal transition duration-200 hover:translate-x-1 ${isActive('/docs/cli') ? 'text-sky-600 hover:text-sky-500' : ''}`}
+              >
+                CLI
+                <Tag text='New' />
+              </Button>
+            </Link>
+          </div>
+        </div>
+        {/* Frameworks */}
+        <div className="py-1">
+          <h2 className="mb-2 px-2 text-base font-medium tracking-tight text-zinc-950 dark:text-zinc-50">Frameworks</h2>
+          <div className="space-y-1">
+            {['Next.js', 'Astro', 'Vite', 'Laravel', 'React', 'Angular', 'Svelte', 'Vue.js'].map((framework) => (
+              <Link key={framework} href="/docs/frameworks" onClick={closeMobileMenu}>
+                <Button variant="ghost" className="w-full justify-start font-normal transition duration-200 hover:translate-x-1">
+                  {framework}
+                </Button>
+              </Link>
+            ))}
+          </div>
+        </div>
+        {/* Components */}
+        <div className="py-1">
+          <h2 className="mb-2 px-2 text-base font-medium tracking-tight text-zinc-950 dark:text-zinc-50">Components</h2>
+          <div className="space-y-1">
+            {['Accordion', 'Autocomplete', 'Avatar', 'Button', 'Badge', 'Calendar', 'Card', 'Checkbox'].map((component) => (
+              <Button key={component} variant="ghost" className="w-full justify-start font-normal transition duration-200 hover:translate-x-1">
+                {component}
+              </Button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
   return (
     <header className="sticky top-0 z-50 w-full bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60 dark:border-[#27272A] dark:bg-black/95 dark:supports-[backdrop-filter]:black/60">
         <div className="container flex h-14 items-center">
-          <div className="mr-4 hidden md:flex md:flex-1">
+          <div className="2xl:m-4 m-8 hidden md:flex md:flex-1">
             <Link className="mr-6 flex items-center space-x-2" href="/">
               <span className="hidden font-bold sm:inline-block">Stofli/UI</span>
             </Link>
@@ -69,42 +136,20 @@ const Header = () => {
             </nav>
           </div>
           <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
-            <SheetTrigger asChild>
-              <Button variant="ghost" size="icon" className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden">
-                <MenuIcon className="h-5 w-5" />
-                <span className="sr-only">Toggle Menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="w-[300px] sm:w-[400px] bg-white dark:bg-zinc-950">
-              <nav className="flex flex-col space-y-4">
-                <Link className="text-lg font-medium hover:text-primary dark:text-white dark:hover:text-primary" href="/docs" onClick={closeMobileMenu}>
-                  Docs
-                </Link>
-                <Link className="text-lg font-medium hover:text-primary dark:text-white dark:hover:text-primary" href="/docs/components" onClick={closeMobileMenu}>
-                  Components
-                </Link>
-                <Link className="text-lg font-medium hover:text-primary dark:text-white dark:hover:text-primary" href="/themes" onClick={closeMobileMenu}>
-                  Themes
-                </Link>
-                <Link className="text-lg font-medium hover:text-primary dark:text-white dark:hover:text-primary" href="/examples" onClick={closeMobileMenu}>
-                  Examples
-                </Link>
-                <Link className="text-lg font-medium hover:text-primary dark:text-white dark:hover:text-primary" href="/blocks" onClick={closeMobileMenu}>
-                  Blocks
-                </Link>
-                <Link className="text-lg font-medium hover:text-primary dark:text-white dark:hover:text-primary" href="/charts" onClick={closeMobileMenu}>
-                  Charts
-                </Link>
-                <Link className="text-lg font-medium hover:text-primary dark:text-white dark:hover:text-primary" href="/colors" onClick={closeMobileMenu}>
-                  Colors
-                </Link>
-              </nav>
-              <SheetClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-zinc-100 dark:ring-offset-zinc-950 dark:focus:ring-zinc-50 dark:data-[state=open]:bg-zinc-800">
-                <XIcon className="h-4 w-4 dark:text-white" />
-                <span className="sr-only">Close</span>
-              </SheetClose>
-            </SheetContent>
-          </Sheet>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon" className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden">
+              <MenuIcon className="h-5 w-5" />
+              <span className="sr-only">Toggle Menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-[300px] sm:w-[400px] bg-white dark:bg-zinc-950 p-0">
+            <SideBar />
+            <SheetClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-zinc-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-zinc-100 dark:ring-offset-zinc-950 dark:focus:ring-zinc-50 dark:data-[state=open]:bg-zinc-800">
+              <XIcon className="h-4 w-4 dark:text-white" />
+              <span className="sr-only">Close</span>
+            </SheetClose>
+          </SheetContent>
+        </Sheet>
           <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
             <div className="w-full flex-1 md:w-auto md:flex-none">
               <Button variant="ghost" size="icon" className="md:hidden" onClick={toggleSearch}>
