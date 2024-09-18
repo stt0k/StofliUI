dfa794bafb8325c7e803edfdeffa95e7c084719d
Stok <espstok04@gmail.com>
2024-09-18 19:58:19 +0200

    header update

---
+++ components/ui/search-data.tsx
@@ -0,0 +1,13 @@
+export interface SearchItem {
+    title: string;
+    link: string;
+  }
+  
+  export const searchItems: SearchItem[] = [
+    { title: "Documentation", link: "/docs" },
+    { title: "Components", link: "/components" },
+    { title: "Blocks", link: "/blocks" },
+    { title: "Charts", link: "/charts" },
+    { title: "Themes", link: "/themes" },
+    { title: "Examples", link: "/examples" },
+  ];
