6039338d1043645e4a2a6e78f9dc7be85e2094a7
Stok <espstok04@gmail.com>
2024-11-05 17:50:57 +0100

    buttonHome & tagHome fix

--- components/home/TagHome.tsx
+++ components/home/TagHome.tsx
@@ -7,12 +7,12 @@ import { HiOutlineSparkles } from "react-icons/hi2";
 const TagHome = () => {
   return (
     <div>
-      <button className='flex justify-center items-center mb-8 gap-x-2 rounded-full dark:bg-purple-900/20 dark:hover:bg-purple-800/30 px-4 py-1 text-lg transition-all duration-300 ease-in-out border border-purple-900'>
-        <span className='text-lg dark:text-purple-400'>
+      <button className='flex justify-center items-center mb-8 gap-x-2 rounded-full dark:bg-purple-900/20 dark:hover:bg-purple-800/30 bg-purple-600/20 hover:bg-purple-500/40 px-4 py-1 text-lg transition-all duration-300 ease-in-out border dark:border-purple-900 border-purple-600'>
+        <span className='text-lg dark:text-purple-400 text-purple-700'>
             <HiOutlineSparkles />
         </span>
-        <span className='text-sm dark:text-purple-400'>Learn what's new</span>
-        <span className='text-lg dark:text-purple-400'>
+        <span className='text-sm dark:text-purple-400 text-purple-700'>Learn what's new</span>
+        <span className='text-lg dark:text-purple-400 text-purple-700'>
             <HiArrowRight />
         </span>
       </button>
