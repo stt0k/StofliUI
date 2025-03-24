1053d7c371ffcc32b29748de0e4e21df355e2eac
Héctor <hectorgilfuertes12@gmail.com>
2025-03-24 21:53:46 +0100

    Refactorizar el componente CLI para incluir elementos de navegación y mejorar la estructura del código. Se añadieron identificadores a las secciones de Uso, Agregar y Ejemplo.

--- app/docs/cli/page.tsx
+++ app/docs/cli/page.tsx
@@ -7,41 +7,50 @@ import Section from '@/app/docs/cli/sections/Section';
 import CommandCLI from '@/app/docs/cli/sections/CommandCLI';
 import TagsCLI from '@/app/docs/cli/sections/TagsCLI';
 
-const Cli: React.FC = () => (
-  <MainLayout>
-    <ContentComponent
-      title="CLI" 
-      description="Installing stofli/ui with the CLI"
-    />
+const Cli: React.FC = () => {
+  // Definimos los elementos de navegación para esta página
+  const navItems = [
+    { title: "Usage", href: "#usage" },
+    { title: "Add", href: "#add" },
+    { title: "Example", href: "#example" }
+  ];
 
-    <div className="space-y-12 mt-7">
-      <Section title="Usage">
-        <CommandCLI command="npx stofli-ui@latest init" />
-        <p>
-          <TagsCLI text="init" /> command initializes a <TagsCLI text="components.json" /> file and a <TagsCLI text="tailwind.config.js" /> file for a new project.
-          It installs <TagsCLI text="framer-motion" />, <TagsCLI text="cn" /> and other dependencies, compatible with shadcnui.
-        </p>
-      </Section>
+  return (
+    <MainLayout navItems={navItems}>
+      <ContentComponent
+        title="CLI" 
+        description="Installing stofli/ui with the CLI"
+      />
 
-      <Section title="Add">
-        <CommandCLI command="npx stofli-ui@latest add [component]" />
-        <p>
-        Adds a new component to your project.
-        </p>
-      </Section>
+      <div className="space-y-12 mt-7">
+        <Section title="Usage" id="usage">
+          <CommandCLI command="npx stofli-ui@latest init" />
+          <p>
+            <TagsCLI text="init" /> command initializes a <TagsCLI text="components.json" /> file and a <TagsCLI text="tailwind.config.js" /> file for a new project.
+            It installs <TagsCLI text="framer-motion" />, <TagsCLI text="cn" /> and other dependencies, compatible with shadcnui.
+          </p>
+        </Section>
 
-      <Section title="Example">
-        <CommandCLI command="npx stofli-ui@latest add bento-grid" />
-        <p className="mb-4">
-          You can also use the optional <TagsCLI text="--all" /> flag to install all components:
-        </p>
-        <CommandCLI command="npx stofli-ui@latest add --all" />
-        <p>
-          You can install the related demos/examples for the related components by using the <TagsCLI text="-e" /> flag with the <TagsCLI text="add" /> command.
-        </p>
-      </Section>
-    </div>
-  </MainLayout>
-);
+        <Section title="Add" id="add">
+          <CommandCLI command="npx stofli-ui@latest add [component]" />
+          <p>
+          Adds a new component to your project.
+          </p>
+        </Section>
+
+        <Section title="Example" id="example">
+          <CommandCLI command="npx stofli-ui@latest add bento-grid" />
+          <p className="mb-4">
+            You can also use the optional <TagsCLI text="--all" /> flag to install all components:
+          </p>
+          <CommandCLI command="npx stofli-ui@latest add --all" />
+          <p>
+            You can install the related demos/examples for the related components by using the <TagsCLI text="-e" /> flag with the <TagsCLI text="add" /> command.
+          </p>
+        </Section>
+      </div>
+    </MainLayout>
+  );
+};
 
 export default Cli;
