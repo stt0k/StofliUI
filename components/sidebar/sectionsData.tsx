39aeae90b735c56bfaf8e5e5bc84949b7804f9e0
stt0k <espstok04@gmail.com>
2025-03-28 03:30:05 +0100

    Actualizar la estructura de datos en sectionsData.tsx para incluir nuevos componentes: Tabs, Dropdown e Input. Se mejoró la legibilidad del código al unificar el formato de comillas y se añadieron nuevos enlaces en la sección de componentes.

--- components/sidebar/sectionsData.tsx
+++ components/sidebar/sectionsData.tsx
@@ -1,33 +1,35 @@
 export const sections = [
-    {
-      title: 'Getting Started',
-      links: [
-        { href: '/docs/introduction', label: 'Introduction' },
-        { href: '/docs/installation', label: 'Installation' },
-        { href: '/docs/cli', label: 'CLI', tag: 'New' },
-      ],
-    },
-    {
-      title: 'Frameworks',
-      links: [
-        { href: '/docs/frameworks/nextjs', label: 'Next.js' },
-        { href: '/docs/frameworks/astro', label: 'Astro' },
-        { href: '/docs/frameworks/vite', label: 'Vite' },
-        { href: '/docs/frameworks/react', label: 'React' },
-      ],
-    },
-    {
-      title: 'Components',
-      links: [
-        { href: '/docs/components/accordion', label: 'Accordion' },
-        { href: '/docs/components/autocomplete', label: 'Autocomplete' },
-        { href: '/docs/components/avatar', label: 'Avatar' },
-        { href: '/docs/components/button', label: 'Button' },
-        { href: '/docs/components/badge', label: 'Badge' },
-        { href: '/docs/components/calendar', label: 'Calendar' },
-        { href: '/docs/components/card', label: 'Card' },
-        { href: '/docs/components/checkbox', label: 'Checkbox' },
-      ],
-    },
-  ];
-  
+  {
+    title: "Getting Started",
+    links: [
+      { href: "/docs/introduction", label: "Introduction" },
+      { href: "/docs/installation", label: "Installation" },
+      { href: "/docs/cli", label: "CLI", tag: "New" },
+    ],
+  },
+  {
+    title: "Frameworks",
+    links: [
+      { href: "/docs/frameworks/nextjs", label: "Next.js" },
+      { href: "/docs/frameworks/astro", label: "Astro" },
+      { href: "/docs/frameworks/vite", label: "Vite" },
+      { href: "/docs/frameworks/react", label: "React" },
+    ],
+  },
+  {
+    title: "Components",
+    links: [
+      { href: "/docs/components/accordion", label: "Accordion" },
+      { href: "/docs/components/autocomplete", label: "Autocomplete" },
+      { href: "/docs/components/avatar", label: "Avatar" },
+      { href: "/docs/components/button", label: "Button" },
+      { href: "/docs/components/badge", label: "Badge" },
+      { href: "/docs/components/calendar", label: "Calendar" },
+      { href: "/docs/components/card", label: "Card" },
+      { href: "/docs/components/checkbox", label: "Checkbox" },
+      { href: "/docs/components/tabs", label: "Tabs" },
+      { href: "/docs/components/dropdown", label: "Dropdown" },
+      { href: "/docs/components/input", label: "Input" },
+    ],
+  },
+];
