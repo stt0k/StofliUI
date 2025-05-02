f095fab4b13c1ebba1d137f7bdcc769a3e5d8b08
stt0k <espstok04@gmail.com>
2025-05-02 02:12:48 +0200

    Actualizar estilos del componente Tag en Tags.tsx, cambiando los colores a una paleta personalizada (#FF5733) para mejorar la estética y la coherencia visual en la interfaz de usuario.

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -6,7 +6,7 @@ interface TagProps {
 
 const Tag: React.FC<TagProps> = ({ text }) => {
   return (
-    <span className="ml-2 rounded-full dark:bg-cyan-900/20 dark:hover:bg-cyan-800/30 px-2 py-0.5 text-xs font-semibold dark:text-cyan-500 bg-cyan-600/20 hover:bg-cyan-500/40 text-cyan-600 transition-all duration-300 ease-in-out">
+    <span className="ml-2 rounded-full dark:bg-[#FF5733]/5 dark:hover:bg-[#FF5733]/10 px-2 py-0.5 text-xs font-semibold dark:text-[#FF5733] bg-[#FF5733]/5 hover:bg-[#FF5733]/40 text-[#FF5733] transition-all duration-300 ease-in-out">
       {text}
     </span>
   );
