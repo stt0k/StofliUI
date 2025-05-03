61341840a0dd574c03c152b5b4ba5570d48f76e1
stt0k <espstok04@gmail.com>
2025-05-03 22:21:58 +0200

    Refactorizar el archivo HeaderData.tsx para unificar el estilo de comillas en las propiedades de los enlaces. Se mejora la consistencia del código y se eliminan entradas innecesarias, manteniendo solo las secciones relevantes.

--- components/header/HeaderData.tsx
+++ components/header/HeaderData.tsx
@@ -1,34 +1,25 @@
 export const headerData = [
-    {
-        title: 'Docs',
-        link: '/docs/introduction'
-    },
-    {
-        title: 'Components',
-        link: '/docs/components/accordion'
-    },
-    {
-        title: 'Themes',
-        link: '/docs/themes'
-    },
-    {
-        title: 'Examples',
-        link: '/docs/examples',
-        hidden: true
-    },
-    {
-        title: 'Blocks',
-        link: '/docs/blocks',
-        hidden: true
-    },
-    {
-        title: 'Changelog',
-        link: '/docs/Changelog',
-        hidden: true
-    },
-    {
-        title: 'Colors',
-        link: '/docs/colors',
-        hidden: true
-    },
-]
+  {
+    title: "Docs",
+    link: "/docs/introduction",
+  },
+  {
+    title: "Components",
+    link: "/docs/components/accordion",
+  },
+  {
+    title: "Examples",
+    link: "/docs/examples",
+    hidden: true,
+  },
+  {
+    title: "Changelog",
+    link: "/docs/Changelog",
+    hidden: true,
+  },
+  {
+    title: "Colors",
+    link: "/docs/colors",
+    hidden: true,
+  },
+];
