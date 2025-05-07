ce7bc6ece11a792b582e7535c82656f329f967a9
stt0k <espstok04@gmail.com>
2025-05-07 21:18:42 +0200

    Agregar rutas relacionadas y comodines en el componente HeaderData para mejorar la navegación y facilitar la expansión futura de la documentación.

--- components/header/HeaderData.tsx
+++ components/header/HeaderData.tsx
@@ -2,24 +2,37 @@ export const headerData = [
   {
     title: "Docs",
     link: "/docs/introduction",
+    relatedPaths: [
+      "/docs/introduction",
+      "/docs/installation",
+      //"/docs/frameworks/nextjs",
+      "/docs/frameworks*", // Comodín para futuras frameworks
+    ],
   },
   {
     title: "Components",
     link: "/docs/components/accordion",
+    relatedPaths: [
+      //"/docs/components/accordion",
+      "/docs/components*", // Comodín para cualquier ruta de componentes
+    ],
   },
   {
     title: "Examples",
     link: "/docs/examples",
     hidden: true,
+    relatedPaths: ["/docs/examples"],
   },
   {
     title: "Changelog",
     link: "/docs/changelog",
     hidden: true,
+    relatedPaths: ["/docs/changelog"],
   },
   {
     title: "Colors",
     link: "/docs/colors",
     hidden: true,
+    relatedPaths: ["/docs/colors"],
   },
 ];
