48b0697d1aad0f8f4c60ea8cc7b8fa651d41c703
Héctor <hectorgilfuertes12@gmail.com>
2025-05-08 20:31:16 +0200

    Actualizar el componente FooterDocs para mejorar la localización, cambiando el texto a "Todos los derechos reservados" y ajustando el color del texto para mayor legibilidad en diferentes temas.

--- components/footer/FooterDocs.tsx
+++ components/footer/FooterDocs.tsx
@@ -2,12 +2,13 @@ import React from "react";
 const Footer = () => {
   return (
     <footer
-      className="bg-white dark:bg-black mt-44 text-white py-4 z-50 border-t border-dashed border-gray-400/20"
+      className="bg-white dark:bg-black mt-44 py-4 z-50 border-t border-dashed border-gray-400/20 text-black dark:text-white"
       role="contentinfo"
     >
       <div className="container mx-auto flex flex-col items-center">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} stofli/ui. All rights reserved.
+          &copy; {new Date().getFullYear()} stofli/ui. Todos los derechos
+          reservados.
         </p>
       </div>
     </footer>
