93c5045448e5a0efefce4fe02ca1f0fc7be1a34c
Héctor <hectorgilfuertes12@gmail.com>
2025-05-11 20:30:10 +0200

    Agregar componente de migas de pan al `ContentComponent`, mejorando la navegación y la estructura visual. Se eliminó el icono de ChevronRight y se configuraron las migas de pan según la ruta actual.

--- components/ContentComponent.tsx
+++ components/ContentComponent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Breadcrumbs, { BreadcrumbItem } from "./sections/breadcrumbs";
 
 interface ContentComponentProps {
   title: string;
@@ -11,16 +12,25 @@ const ContentComponent: React.FC<ContentComponentProps> = ({
   title,
   description,
 }) => {
+  // Configurar las migas de pan según la ruta
+  const breadcrumbItems: BreadcrumbItem[] = [
+    { label: "Docs", href: "/docs/introduccion" },
+  ];
+
+  breadcrumbItems.push({
+    label: title,
+    className: "!text-cyan-600 dark:!text-cyan-500",
+  });
+
   return (
     <div className="flex flex-col ms-6">
-      <div className="mb-4 mt-11 flex items-center space-x-1 text-sm text-zinc-500 dark:text-zinc-400">
-        <div className="overflow-hidden text-ellipsis whitespace-nowrap">
-          Docs
-        </div>
-        <ChevronRightIcon className="h-4 w-4" />
-        <div className="font-medium text-cyan-600 dark:text-cyan-500">
-          {title}
-        </div>
+      <div className="mb-4 mt-11">
+        <Breadcrumbs
+          items={breadcrumbItems}
+          variant="default"
+          size="lg"
+          clickable={true}
+        />
       </div>
       <div className="space-y-2">
         <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">
@@ -33,24 +43,5 @@ const ContentComponent: React.FC<ContentComponentProps> = ({
     </div>
   );
 };
-
-function ChevronRightIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M9 18l6-6-6-6" />
-    </svg>
-  );
-}
 
 export default ContentComponent;
