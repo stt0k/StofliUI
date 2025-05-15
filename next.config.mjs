bdb91d4aca32ef69a0d539031f5a5051392a10e1
Héctor <hectorgilfuertes12@gmail.com>
2025-05-15 21:15:50 +0200

    Integrar Contentlayer en la configuración de Next.js, mejorando la gestión de documentos MDX al envolver la configuración existente con `withContentlayer`.

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withContentlayer } from 'next-contentlayer2';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -14,4 +16,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
