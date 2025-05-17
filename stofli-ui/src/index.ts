a5b53431661f0948eafc3f39c81bcfe88f1e949e
Héctor <hectorgilfuertes12@gmail.com>
2025-05-17 20:34:48 +0200

    Agregar archivo index.ts en "stofli-ui" para exportar utilidades y componentes principales, facilitando la importación y organización de la biblioteca de componentes UI.

---
+++ stofli-ui/src/index.ts
@@ -0,0 +1,25 @@
+// Exportar utilidades
+export { cn } from "./lib/utils";
+
+// Exportar componentes principales
+export { default as Accordion } from "./components/accordion";
+export { default as Avatar } from "./components/avatar";
+export { default as Autocomplete } from "./components/autocomplete";
+export { default as Badge } from "./components/badge";
+export { default as Breadcrumbs } from "./components/breadcrumbs";
+export { default as Button } from "./components/button";
+export { default as Calendar } from "./components/calendar";
+export { default as Card } from "./components/card";
+export { default as Checkbox } from "./components/checkbox";
+export { default as CircularProgress } from "./components/circular-progress";
+export { default as DatePicker } from "./components/date-picker";
+export { default as Dropdown } from "./components/dropdown";
+export { default as Input } from "./components/input";
+export { default as NumberInput } from "./components/number-input";
+export { default as Pagination } from "./components/pagination";
+export { default as Progress } from "./components/progress";
+export { default as Spinner } from "./components/spinner";
+export { default as Switch } from "./components/switch";
+export { default as Tabs } from "./components/tabs";
+export { Toast, ToastProvider, useToast } from "./components/toast";
+export { default as Tooltip } from "./components/tooltip";
