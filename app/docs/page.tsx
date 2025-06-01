03f7891bad1824a8476639590a4996bdcc19068b
Héctor <hectorgilfuertes12@gmail.com>
2025-06-01 13:59:31 +0200

    Agregar la página de documentación DocsPage que redirige automáticamente a la sección de introducción de primeros pasos. Esto mejora la navegación y la accesibilidad de la documentación.

---
+++ app/docs/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from "next/navigation";
+
+export default function DocsPage() {
+  // Redireccionar de /docs a /docs/primeros-pasos/introduccion
+  redirect("/docs/primeros-pasos/introduccion");
+} 
