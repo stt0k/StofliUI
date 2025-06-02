515685d1314670cfc5c54661cb2098306767e2ea
stt0k <espstok04@gmail.com>
2025-06-03 01:04:45 +0200

    Refactorizar el componente SheetDescription, actualizando la clase de estilo de 'dark:text-zinc-400' a 'dark:text-neutral-400' para mejorar la coherencia visual. Se optimizó la legibilidad del código al mantener la estructura del componente.

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -129,7 +129,7 @@ const SheetDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Description
     ref={ref}
-    className={cn("text-sm text-zinc-500 dark:text-zinc-400", className)}
+    className={cn("text-sm text-zinc-500 dark:text-neutral-400", className)}
     {...props}
   />
 ));
