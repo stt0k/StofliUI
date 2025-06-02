20c9d5a1823769afbefa1b66f306e9ba91e708b2
stt0k <espstok04@gmail.com>
2025-06-02 18:20:05 +0200

    Actualizar next.config.mjs para unificar el estilo de comillas en las configuraciones de imágenes y añadir un nuevo patrón remoto para 'avatars.githubusercontent.com', mejorando la consistencia y la funcionalidad de la configuración.

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,24 @@
-import { withContentlayer } from 'next-contentlayer2';
+import { withContentlayer } from "next-contentlayer2";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
       },
       {
-        protocol: 'https',
-        hostname: 'randomuser.me',
+        protocol: "https",
+        hostname: "randomuser.me",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
       },
     ],
   },
