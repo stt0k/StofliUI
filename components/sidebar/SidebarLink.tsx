9d4708819d41388e6cd0f5134088e1ed139e93de
Héctor <hectorgilfuertes12@gmail.com>
2025-06-03 22:03:00 +0200

    Actualizar el componente SidebarLink para permitir la inclusión de iconos y enlaces principales. Se mejoró la truncación del texto y se ajustaron los estilos para una mejor presentación visual y navegación.

--- components/sidebar/SidebarLink.tsx
+++ components/sidebar/SidebarLink.tsx
@@ -1,35 +1,64 @@
 import React from "react";
 import Link from "next/link";
 import Tag from "@/components/Tags";
+import { Button } from "@/components/ui/button";
 
 type SidebarLinkProps = {
   href: string;
   label: string;
   isActive: boolean;
   tag?: string;
+  icon?: React.ReactNode;
+  isMainLink?: boolean;
 };
 
-const SidebarLink = ({ href, label, isActive, tag }: SidebarLinkProps) => {
+const SidebarLink = ({ href, label, isActive, tag, icon, isMainLink = false }: SidebarLinkProps) => {
   // Función para truncar el texto a 11 caracteres y añadir ...
   const truncateText = (text: string) => {
-    if (text.length > 11) {
-      return text.substring(0, 11) + "...";
+    if (text.length > 20) {
+      return text.substring(0, 20) + "...";
     }
     return text;
   };
-
+  
   return (
-    <Link href={href} className="block">
-      <button
-        className={`w-full text-left flex items-center pl-2 py-2 rounded-md text-sm transition-all duration-300 ease-in-out disabled:pointer-events-none disabled:opacity-50 hover:bg-zinc-100 dark:hover:bg-zinc-800 justify-start font-normal cursor-pointer hover:translate-x-1 dark:hover:text-zinc-50/80 dark:text-zinc-50/60 text-zinc-950/60 hover:text-zinc-950/85 ${
-          isActive
-            ? "hover:text-zinc-950/85 text-zinc-950 dark:hover:text-zinc-50/80 dark:text-zinc-50 hover:translate-x-0"
-            : ""
+    <Link 
+      href={href} 
+      className={`block group ${
+        isMainLink 
+          ? "mb-1" 
+          : `border-s-[1px] py-1 ${
+              isActive 
+                ? "border-cyan-400 dark:border-cyan-500" 
+                : "border-neutral-200 dark:border-white/20 hover:border-neutral-400 dark:hover:border-white/80"
+            }`
+      }`}
+    >
+      <Button
+        variant="ghost"
+        className={`w-full justify-start font-medium transition duration-200 cursor-pointer h-auto py-0 hover:bg-transparent dark:hover:bg-transparent ${
+          isMainLink 
+            ? "gap-2 mb-2.5 dark:text-neutral-50 text-neutral-950" 
+            : ` ${
+                isActive
+                ? "text-cyan-400 dark:text-cyan-500 group-hover:text-cyan-400 group-hover:dark:text-cyan-500 pl-3 pr-2"
+                : "pl-3 pr-2 dark:text-neutral-50/60 text-neutral-950/60 group-hover:text-neutral-950/85 group-hover:dark:text-neutral-50/90 hover:bg-transparent"
+              }`
         }`}
       >
-        <span className="truncate">{tag ? truncateText(label) : label}</span>
+        {icon && (
+          <span className={`-ml-4 ${isMainLink 
+            ? "flex items-center justify-center w-8 h-8 rounded-sm border border-neutral-200 dark:border-white/20" 
+            : ""}`}
+          >
+            {icon}
+          </span>
+        )}
+        <span className={`truncate ${isMainLink && "group-hover:text-cyan-600 group-hover:dark:text-cyan-500"}`}>
+          {tag ? truncateText(label) : label}
+        </span>
         {tag && <Tag text={tag} />}
-      </button>
+      </Button>
     </Link>
   );
 };
