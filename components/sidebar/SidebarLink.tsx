17f773b408d73e26fba512cbd90e0d28b605c239
stt0k <espstok04@gmail.com>
2025-06-04 16:58:48 +0200

    Refactorizar el componente SidebarLink para mejorar la legibilidad del código. Se ajustó el formato y la indentación de las propiedades del componente, optimizando así su estructura y presentación visual.

--- components/sidebar/SidebarLink.tsx
+++ components/sidebar/SidebarLink.tsx
@@ -12,7 +12,14 @@ type SidebarLinkProps = {
   isMainLink?: boolean;
 };
 
-const SidebarLink = ({ href, label, isActive, tag, icon, isMainLink = false }: SidebarLinkProps) => {
+const SidebarLink = ({
+  href,
+  label,
+  isActive,
+  tag,
+  icon,
+  isMainLink = false,
+}: SidebarLinkProps) => {
   // Función para truncar el texto a 11 caracteres y añadir ...
   const truncateText = (text: string) => {
     if (text.length > 20) {
@@ -20,16 +27,16 @@ const SidebarLink = ({ href, label, isActive, tag, icon, isMainLink = false }: S
     }
     return text;
   };
-  
+
   return (
-    <Link 
-      href={href} 
+    <Link
+      href={href}
       className={`block group ${
-        isMainLink 
-          ? "mb-1" 
+        isMainLink
+          ? "mb-1"
           : `border-s-[1px] py-1 ${
-              isActive 
-                ? "border-cyan-400 dark:border-cyan-500" 
+              isActive
+                ? "border-cyan-400 dark:border-cyan-500"
                 : "border-neutral-200 dark:border-white/20 hover:border-neutral-400 dark:hover:border-white/80"
             }`
       }`}
@@ -37,24 +44,32 @@ const SidebarLink = ({ href, label, isActive, tag, icon, isMainLink = false }: S
       <Button
         variant="ghost"
         className={`w-full justify-start font-medium transition duration-200 cursor-pointer h-auto py-0 hover:bg-transparent dark:hover:bg-transparent ${
-          isMainLink 
-            ? "gap-2 mb-2.5 dark:text-neutral-50 text-neutral-950" 
+          isMainLink
+            ? "gap-2 mb-2.5 dark:text-neutral-50 text-neutral-950"
             : ` ${
                 isActive
-                ? "text-cyan-400 dark:text-cyan-500 group-hover:text-cyan-400 group-hover:dark:text-cyan-500 pl-3 pr-2"
-                : "pl-3 pr-2 dark:text-neutral-50/60 text-neutral-950/60 group-hover:text-neutral-950/85 group-hover:dark:text-neutral-50/90 hover:bg-transparent"
+                  ? "text-cyan-400 dark:text-cyan-500 group-hover:text-cyan-400 group-hover:dark:text-cyan-500 pl-3 pr-2"
+                  : "pl-3 pr-2 dark:text-neutral-50/60 text-neutral-950/60 group-hover:text-neutral-950/85 group-hover:dark:text-neutral-50/90 hover:bg-transparent"
               }`
         }`}
       >
         {icon && (
-          <span className={`-ml-4 ${isMainLink 
-            ? "flex items-center justify-center w-8 h-8 rounded-sm border border-neutral-200 dark:border-white/20" 
-            : ""}`}
+          <span
+            className={`-ml-4 ${
+              isMainLink
+                ? "flex items-center justify-center w-8 h-8 rounded-sm border border-neutral-200 dark:border-white/20"
+                : ""
+            }`}
           >
             {icon}
           </span>
         )}
-        <span className={`truncate ${isMainLink && "group-hover:text-cyan-600 group-hover:dark:text-cyan-500"}`}>
+        <span
+          className={`truncate ${
+            isMainLink &&
+            "group-hover:text-cyan-600 group-hover:dark:text-cyan-500"
+          }`}
+        >
           {tag ? truncateText(label) : label}
         </span>
         {tag && <Tag text={tag} />}
