fc7589a39d84253cbcb97d5f98284157c1c0c6af
stt0k <espstok04@gmail.com>
2025-06-05 02:07:14 +0200

    Actualizar estilos en el componente SidebarLink para mejorar la presentación visual. Se ajustaron las clases de margen, tamaño y color, optimizando así la coherencia visual y la interacción del usuario.

--- components/sidebar/SidebarLink.tsx
+++ components/sidebar/SidebarLink.tsx
@@ -33,7 +33,7 @@ const SidebarLink = ({
       href={href}
       className={`block group ${
         isMainLink
-          ? "mb-1"
+          ? "mb-1 hover:opacity-100"
           : `border-s-[1px] py-1 ${
               isActive
                 ? "border-cyan-600 dark:border-cyan-500"
@@ -43,12 +43,12 @@ const SidebarLink = ({
     >
       <Button
         variant="ghost"
-        className={`w-full justify-start font-medium transition duration-200 cursor-pointer h-auto py-0 hover:bg-transparent dark:hover:bg-transparent ${
+        className={`w-full justify-start font-normal transition duration-200 cursor-pointer h-auto py-0 hover:bg-transparent dark:hover:bg-transparent ${
           isMainLink
-            ? "gap-2 mb-2.5 dark:text-neutral-50 text-neutral-950"
+            ? "gap-3 mb-2.5 dark:text-neutral-50/60 text-neutral-950/60 group-hover:opacity-100"
             : ` ${
                 isActive
-                  ? "text-cyan-600 dark:text-cyan-500 group-hover:text-cyan-400 group-hover:dark:text-cyan-500 pl-3 pr-2"
+                  ? "text-cyan-600 dark:text-cyan-500 0 pl-3 pr-2"
                   : "pl-3 pr-2 dark:text-neutral-50/60 text-neutral-950/60 group-hover:text-neutral-950/85 group-hover:dark:text-neutral-50/90 hover:bg-transparent"
               }`
         }`}
@@ -57,7 +57,7 @@ const SidebarLink = ({
           <span
             className={`-ml-4 ${
               isMainLink
-                ? "flex items-center justify-center w-8 h-8 rounded-sm border border-neutral-200 dark:border-white/20"
+                ? "flex items-center justify-center w-6 h-6 rounded-sm p-1 shadow-sm dark:brightness-[1.35] dark:hover:brightness-150 group-hover:brightness-100 group-hover:ring-0 ring-1 ring-neutral-950/5 dark:ring-neutral-700/80 text-neutral-600 dark:text-white dark:opacity-50 opacity-70 group-hover:opacity-100 dark:group-hover:opacity-100 group-hover:text-neutral-950 dark:group-hover:text-neutral-400 transition-all duration-200"
                 : ""
             }`}
           >
@@ -67,7 +67,7 @@ const SidebarLink = ({
         <span
           className={`truncate ${
             isMainLink &&
-            "group-hover:text-cyan-600 group-hover:dark:text-cyan-500"
+            "group-hover:text-neutral-950/85 group-hover:dark:text-neutral-50/90 transition-all duration-200"
           }`}
         >
           {tag ? truncateText(label) : label}
