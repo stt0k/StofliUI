3a5ecbdce0cd5e912dc37f8f858396508efb7104
stt0k <espstok04@gmail.com>
2025-06-11 14:09:33 +0200

    Modificar la redirección en la página de documentación. Se actualizó la ruta de redirección de /docs/primeros-pasos/introduccion a /docs/introduccion.

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function DocsPage() {
   // Redireccionar de /docs a /docs/primeros-pasos/introduccion
-  redirect("/docs/primeros-pasos/introduccion");
-} 
+  redirect("/docs/introduccion");
+}
